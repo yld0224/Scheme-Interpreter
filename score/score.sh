715e7aeaf9250f4732859d0f625079f46b71bf8a
yld0224 <yld0224@sjtu.edu.cn>
2025-11-10 18:14:56 +0800

    123

--- score/score.sh
+++ score/score.sh
@@ -7,7 +7,7 @@ echo "--------------------------------------------------------------------------
 cd "$(dirname "$0")"
 
 L=1
-R=118
+R=1
 for ((i = $L; i <= $R; i = i + 1))
 do
     echo ""
@@ -32,40 +32,6 @@ EOF
     diff -b scm.out data/$i.out > diff_output.txt
     if [ $? -ne 0 ]; then
         echo "Wrong answer in TEST" $i
-        # echo "---------------------------"
-        # echo ""
-        # exit 1
-    fi
-    echo "---------------------------"
-    echo ""
-done
-
-L_EXTRA=1
-R_EXTRA=7
-for ((i = $L_EXTRA; i <= $R_EXTRA; i = i + 1))
-do
-    echo ""
-    echo "---------------------------"
-    echo "Ready to test: EXTRA TEST" $i
-    if [ ! -f "more-tests/$i.in" ]; then
-        echo "Input file more-tests/$i.in not found, skipping EXTRA TEST $i"
-        continue
-    fi
-    if [ ! -f "more-tests/$i.out" ]; then
-        echo "Output file more-tests/$i.out not found, skipping EXTRA TEST $i"
-        continue
-    fi
-    ../build/code << EOF > scm.out
-    $(cat more-tests/$i.in)
-    (exit)
-EOF
-    sed '$d' scm.out > scm_cleaned.out
-    mv scm_cleaned.out scm.out
-    sed 's/scm> //' scm.out > scm_cleaned.out
-    mv scm_cleaned.out scm.out
-    diff -b scm.out more-tests/$i.out > diff_output.txt
-    if [ $? -ne 0 ]; then
-        echo "Wrong answer in EXTRA TEST" $i
         # echo "---------------------------"
         # echo ""
         # exit 1
